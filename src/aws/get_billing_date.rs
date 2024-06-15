62bb26ef557dbe179733111a39130ffc1e8a9f0a
yutak03 <yutak03.dev@gmail.com>
2024-06-16 01:04:45 +0900

    Refactored get_billing_date as an implementation of the struct

--- src/aws/get_billing_date.rs
+++ src/aws/get_billing_date.rs
@@ -1,41 +1,43 @@
 use time::{macros::format_description, OffsetDateTime};
 
-// TODO: もうちょっといい感じにかけると思う
 pub struct BillingPeriod {
     pub start_date: String,
     pub end_date: String,
 }
 
-pub fn get_billing_period() -> BillingPeriod {
-    // 現在の日付と時刻を取得
-    let now = OffsetDateTime::now_utc();
+impl BillingPeriod {
+    // 請求期間の取得
+    pub fn get_billing_date() -> Self {
+        // 実行時現在の日時を取得
+        let now = OffsetDateTime::now_utc();
 
-    // 今月の1日の日付を作成
-    let first_day_of_month = now.replace_day(1).expect("Failed to set day to 1");
+        // 実行時の1日の日付を作成
+        let first_day_of_month = now.replace_day(1).expect("Failed to set to first day");
 
-    // 先月の1日を計算
-    let first_day_of_last_month = if first_day_of_month.month() == time::Month::January {
-        first_day_of_month
-            .replace_year(first_day_of_month.year() - 1)
-            .and_then(|dt| dt.replace_month(time::Month::December))
-            .expect("Failed to set to last year's December")
-    } else {
-        first_day_of_month
-            .replace_month(first_day_of_month.month().previous())
-            .expect("Failed to set to previous month")
-    };
+        // 先月の1日を算出
+        let first_day_of_last_month = if first_day_of_month.month() == time::Month::January {
+            first_day_of_month
+                .replace_year(first_day_of_month.year() - 1)
+                .and_then(|dt| dt.replace_month(time::Month::December))
+                .expect("Failed to set to last year")
+        } else {
+            first_day_of_month
+                .replace_month(first_day_of_month.month().previous())
+                .expect("Failed to set to previous month")
+        };
 
-    // 日付をyyyy-mm-dd形式でフォーマット
-    let format = format_description!("[year]-[month]-[day]");
-    let billing_start_date = first_day_of_last_month
-        .format(&format)
-        .expect("Failed to format date");
-    let billing_end_date = first_day_of_month
-        .format(&format)
-        .expect("Failed to format date");
+        // 日付をフォーマット
+        let format = format_description!("[year]-[month]-[day]");
+        let billing_start_date = first_day_of_last_month
+            .format(&format)
+            .expect("Failed to format billing start date");
+        let billing_end_date = first_day_of_month
+            .format(&format)
+            .expect("Failed to format billing end date");
 
-    BillingPeriod {
-        start_date: billing_start_date,
-        end_date: billing_end_date,
+        Self {
+            start_date: billing_start_date,
+            end_date: billing_end_date,
+        }
     }
 }
